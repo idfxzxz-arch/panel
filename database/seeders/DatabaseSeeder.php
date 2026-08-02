e0ec0101c81d84430813ad3efc5f070b02d196c3
idfxzxz-arch <tegardarmawan@gmail.com>
2026-08-02 11:42:26 +0700

    Add admin user with username 'admin' and password 'admin123'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin',
+            'password' => bcrypt('admin123'),
+        ]);
     }
 }
